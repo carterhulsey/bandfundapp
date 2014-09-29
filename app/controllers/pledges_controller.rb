8e7eac6527fecbed417fd87845a0a154c3c75387
Corey Grusden <cgrusden@divergentsoftware.com>
2014-09-29 16:44:33 -0700

    Pledges now save and create relationship wiht artist

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -7,4 +7,18 @@ class PledgesController < ApplicationController
   def show
   end
 
+  def create
+    @pledge = Pledge.create(pledge_params)
+    if @pledge.save
+      ArtistPledge.create(pledge_id: @pledge.id, artist_id: @pledge.artist_id)
+      redirect_to artist_path(@pledge.artist_id)
+    else
+      render "new"
+    end
+  end
+
+  private
+  def pledge_params
+    params.require(:pledge).permit(:artist_id, :fan_id, :credit_card, :price)
+  end
 end
