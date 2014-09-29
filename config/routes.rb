91233cc56ff5bda1bab026e66c110c18e0c40b55
Corey Grusden <cgrusden@divergentsoftware.com>
2014-09-29 12:54:38 -0700

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :fans
-  get "/auth/:provider/callback" => "sessions#create"
+  get "/artists/auth/:provider/callback" => "sessions#create"
   devise_for :artists, controllers: { registrations: "artists/devise/registrations"}
   resources :artists, only: :show do
     resources :rewards, only: [:new, :create, :update]
