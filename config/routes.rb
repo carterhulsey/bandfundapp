0c0fa87fb24a14923f0bc03830850afbe1ed44d8
Corey Grusden <cgrusden@divergentsoftware.com>
2014-09-29 12:21:48 -0700

    Sets up fan model and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :fans
   devise_for :artists, controllers: { :omniauth_callbacks => "artists/devise/omniauth_callbacks", registrations: "artists/devise/registrations"}
   resources :artists, only: :show do
     resources :rewards, only: [:new, :create, :update]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929040045) do
+ActiveRecord::Schema.define(version: 20140929191945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,24 @@ ActiveRecord::Schema.define(version: 20140929040045) do
 
   add_index "artists", ["email"], name: "index_artists_on_email", unique: true, using: :btree
   add_index "artists", ["reset_password_token"], name: "index_artists_on_reset_password_token", unique: true, using: :btree
+
+  create_table "fans", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "fans", ["email"], name: "index_fans_on_email", unique: true, using: :btree
+  add_index "fans", ["reset_password_token"], name: "index_fans_on_reset_password_token", unique: true, using: :btree
 
   create_table "genres", force: true do |t|
     t.string   "name"
