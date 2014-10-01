322ef324c0c715db7ff2a4da407e909f7d485e5a
Corey Grusden <cgrusden@divergentsoftware.com>
2014-10-01 15:51:03 -0700

    Migrates attachment to artist

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141001184921) do
+ActiveRecord::Schema.define(version: 20141001224954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,10 @@ ActiveRecord::Schema.define(version: 20141001184921) do
     t.string   "access_token"
     t.string   "stripe_publishable_key"
     t.string   "stripe_user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "artists", ["email"], name: "index_artists_on_email", unique: true, using: :btree
