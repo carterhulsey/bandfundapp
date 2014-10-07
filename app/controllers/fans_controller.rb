8ba6bbb25800c178780900c99b56ae52c364f814
Corey Grusden <cgrusden@divergentsoftware.com>
2014-10-07 15:47:37 -0700

    Sets up discovery

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -1,6 +1,9 @@
 class FansController < ApplicationController
 
   def index
-    @artists = Artist.all
+    artist_groups = Artist.pluck(:id).sample(9).in_groups_of(3)
+    @artists1 = Artist.where(id: artist_groups[0])
+    @artists2 = Artist.where(id: artist_groups[1])
+    @artists3 = Artist.where(id: artist_groups[2])
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
-    @artists = Artist.all
+    artist_groups = Artist.pluck(:id).sample(9).in_groups_of(3)
+    @artists1 = Artist.where(id: artist_groups[0])
+    @artists2 = Artist.where(id: artist_groups[1])
+    @artists3 = Artist.where(id: artist_groups[2])
     if current_fan
       redirect_to fans_path 
     end
