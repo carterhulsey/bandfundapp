cf0ec276ec7a3a1de6f3adcd2d88c594c4044a2d
Corey Grusden <cgrusden@divergentsoftware.com>
2014-10-07 10:02:34 -0700

    Only a fan can make a pledge

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -7,10 +7,6 @@ class PledgesController < ApplicationController
     if !current_fan 
       render "sign_up"
     end
-    @pledge = Pledge.new
-    @reward = Reward.find_by(:id => params[:reward_id])
-    @artist = Artist.find_by(:id => @reward.artist_id)
-    @rewards = Reward.where(:id => @artist.id)
   end
 
   def show
