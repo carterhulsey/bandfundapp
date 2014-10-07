fe72d29b1aadfe567c6e52ad730c27dd85430827
Corey Grusden <cgrusden@divergentsoftware.com>
2014-10-06 21:45:33 -0700

    Testing stripe curl

--- app/controllers/artists/devise/omniauth_callbacks_controller.rb
+++ app/controllers/artists/devise/omniauth_callbacks_controller.rb
@@ -14,8 +14,7 @@ class Artists::Devise::OmniauthCallbacksController < Devise::OmniauthCallbacksCo
   def stripe_connect
     @user = Artist.find_by(id: current_artist.id)
     @code = params[:code]
-    line = Cocaine::CommandLine.new(`curl -X POST https://connect.stripe.com/oauth/token -d client_secret=#{ENV['STRIPE_SECRET']} -d code=#{@code} -d grant_type=authorization_code`)
-    @response = ActiveSupport::JSON.decode(line.run)
+    @response = ActiveSupport::JSON.decode(`curl -X POST https://connect.stripe.com/oauth/token -d client_secret=#{ENV['STRIPE_SECRET']} -d code=#{@code} -d grant_type=authorization_code`)
     Artist.stripe_params(@response, @user)
     redirect_to root_url
   end
