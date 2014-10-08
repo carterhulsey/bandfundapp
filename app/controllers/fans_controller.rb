cd853727514b6ed6b43210c222a240e317d026b2
Corey Grusden <cgrusden@divergentsoftware.com>
2014-10-08 15:40:06 -0700

    Renders partial in fans dashboard

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -1,6 +1,7 @@
 class FansController < ApplicationController
 
   def index
+    @featured_artist = Artist.find_by(featured: true)
     artist_groups = Artist.pluck(:id).sample(9).in_groups_of(3)
     @artists1 = Artist.where(id: artist_groups[0])
     @artists2 = Artist.where(id: artist_groups[1])
